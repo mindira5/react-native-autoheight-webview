09031600fbcc9f57fc2e452c080232bde8e62acd
Robert <robert20paul@gmail.com>
2017-12-15 10:43:21 -0600

    Resolve android issue for 0.51.0

--- android/src/main/java/com/dscj/autoheightwebview/AutoHeightWebViewPackage.java
+++ android/src/main/java/com/dscj/autoheightwebview/AutoHeightWebViewPackage.java
@@ -6,7 +6,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,11 +14,6 @@ public class AutoHeightWebViewPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
         return new ArrayList<>();
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
     }
 
     @Override
